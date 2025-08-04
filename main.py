da5400a984491ba842e9f00ffc7a13fb207b3a5f
pumpk1n986 <office@tom-haderer.com>
2025-08-04 19:30:54 +0200

    CH2 L5 done

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+from stats import get_num_words
+
+main()
