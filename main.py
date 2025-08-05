f6c81c1dcad9be904c884999f7551b7004c012fe
pumpk1n986 <office@tom-haderer.com>
2025-08-05 16:01:55 +0200

    CH3 L1 done

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+from stats import get_num_words, get_char_counts, convert_to_list
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    char_counts = get_char_counts(text)
+    sorted_chars = convert_to_list(char_counts)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char_dict in sorted_chars:
+        if char_dict["char"].isalpha():
+            print(f"{char_dict['char']}: {char_dict['num']}")
+    print("============= END ===============")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-from stats import get_num_words
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,24 @@
+def sort_count(char_count):
+    return char_count["num"]
+
+def convert_to_list(char_count):
+    num = []
+    for count in char_count:
+        new_dict = {"char": count, "num": char_count[count]}
+        num.append(new_dict)
+    num.sort(reverse = True, key = sort_count)
+    return num
+
+def get_char_counts(text):
+    char_count = {}
+    for char in text:
+        char = char.lower()
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+
 def get_num_words(text):
     words = text.split()
     return len(words)
